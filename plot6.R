104f89eb94e09956b26f8113c5f4273c332003c5
wdsteck <wdsteck@yahoo.com>
2016-07-13 18:18:25 -0400

    Cosmetic changes to plot 5. Added initial files for plot 6.

--- plot6.R
+++ plot6.R
@@ -1,5 +1,5 @@
 #
-# plot5.R script
+# plot6.R script
 #
 # This script performs an exploratory data analysis for the second
 # class project.
@@ -8,8 +8,10 @@
 #       https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip
 #
 # This script answers the following question:
-#       How have emissions from motor vehicle sources changed from
-#	1999–2008 in Baltimore City?
+#       Compare emissions from motor vehicle sources in Baltimore
+#       City with emissions from motor vehicle sources in Los Angeles
+#       County, California (𝚏𝚒𝚙𝚜 == "𝟶𝟼𝟶𝟹𝟽"). Which city has seen
+#	greater changes over time in motor vehicle emissions?
 #
 
 # Download the zip file and uncompress it giving the 2 source files.
@@ -18,7 +20,7 @@ dataFileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.z
 dataFileZip <- "NEI_data.zip"
 dataFileSrcSCC <- "Source_Classification_Code.rds"
 dataFileSrcSUM <- "summarySCC_PM25.rds"
-dataFileOut <- "plot5.png"
+dataFileOut <- "plot6.png"
 
 if ((!file.exists(dataFileSrcSCC)) | (!file.exists(dataFileSrcSUM))) {
         if (!file.exists(dataFileZip)) {
@@ -50,8 +52,9 @@ print("Analyzing the Data")
 # finding the SCC values from Coal Sources
 SCC <- SCC[grepl("Vehicles", SCC$EI.Sector, ignore.case=TRUE), c("SCC", "EI.Sector")]
 
-# pulling those Baltimore City emissions values that are from Motor Vehicle sources
-NEI <- NEI[(NEI$SCC %in% SCC$SCC) & (NEI$fips == "24510"),]
+# pulling those Baltimore City and LA County emissions values
+# that are from Motor Vehicle sources
+NEI <- NEI[(NEI$SCC %in% SCC$SCC) & ((NEI$fips == "24510") | (NEI$fips == "06037")),]
 
 print("Creating Plot")
 
@@ -62,31 +65,62 @@ png(dataFileOut, width = 480, height = 480)
 
 # Find the Emissions over each year of data
 
-aggdata <- aggregate(Emissions ~ year, NEI, sum,
+aggdata <- aggregate(Emissions ~ year+fips, NEI, sum,
                      na.rm = TRUE, na.action="na.pass")
 
 # Plot the data. Fiddle with the axis scales to make the plot look nicer
-plot(aggdata,
-     main = "Total PM2.5 Emmissions from Vehicle Sources\nin the Baltimore City, MD over Time",
+
+plot(x = range(aggdata$year, na.rm = TRUE),
+     y = range(aggdata$Emissions, na.rm = TRUE),
+     main = "Total PM2.5 Emmissions from Vehicle Sources\nin Baltimore City, MD and LA County, CA over Time",
      xlab = "Year",
      ylab = "Emissions (in Tons)",
-     pch = 19,
-     type = "p",
+     xaxt = "n",
      xlim = c(min(aggdata$year, na.rm = TRUE),
               max(aggdata$year, na.rm = TRUE) + 1),
      ylim = c(0, ceiling(max(aggdata$Emissions, na.rm=TRUE))),
-     xaxt = "n"
-)
+     type = "n"
+     )
 
-# only show labels for the years we are plotting on the x axis
+# Add a grid to compare the slopes of the regression lines
+grid(lty = 3, col = "dark grey")
+
+# only show the labels for the years we are plotting on the x axis
 axis(side = 1, at = aggdata$year)
 
-# add a regression line
-abline(lm(aggdata$Emissions ~ aggdata$year), col = "red")
+# Plot the Baltimore City points and linear regression line
+points(aggdata$year[aggdata$fips == "24510"],
+       aggdata$Emissions[aggdata$fips == "24510"],
+       pch = 19,
+       col="red"
+       )
+abline(lm(aggdata$Emissions[aggdata$fips == "24510"] ~
+                  aggdata$year[aggdata$fips == "24510"]),
+       col = "red")
+
+# Plot the LA County points and linear regression line
+points(aggdata$year[aggdata$fips == "06037"],
+       aggdata$Emissions[aggdata$fips == "06037"],
+       pch = 19,
+       col="blue"
+       )
+abline(lm(aggdata$Emissions[aggdata$fips == "06037"] ~
+                  aggdata$year[aggdata$fips == "06037"]),
+       col = "blue")
 
 # add data point labels
 datlab <- round(aggdata$Emissions, digits = 2)
 text(aggdata$year, aggdata$Emissions, datlab, pos=4, cex = .7)
+
+# Add legend
+legend("right", pch = 19, 
+       col = c("red", NA, "blue"), 
+       legend = c("Baltimore City", NA, "LA County"),
+       cex = .8,
+       title = "Emissions",
+       text.width = 2
+       )
+
 
 # Close the device
 dev.off()
