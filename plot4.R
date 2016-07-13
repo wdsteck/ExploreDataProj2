760634268f4b56d641c600134a82da016c5155aa
wdsteck <wdsteck@yahoo.com>
2016-07-13 16:00:31 -0400

    New plot 4 files. minor changes to plot1.R and gitignore.

--- plot4.R
+++ plot4.R
@@ -1,5 +1,5 @@
 #
-# plot1.R script
+# plot4.R script
 #
 # This script performs an exploratory data analysis for the second
 # class project.
@@ -8,11 +8,9 @@
 #       https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip
 #
 # This script answers the following question:
-#	Have total emissions from PM2.5 decreased in the United States
-#	from 1999 to 2008? Using the base plotting system, make a plot
-#	showing the total PM2.5 emission from all sources for each of the
-#	years 1999, 2002, 2005, and 2008.
-#
+#       Across the United States, how have emissions from coal
+#       combustion-related sources changed from 1999–2008?
+
 
 # Download the zip file and uncompress it giving the 2 source files.
 
@@ -20,32 +18,42 @@ dataFileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.z
 dataFileZip <- "NEI_data.zip"
 dataFileSrcSCC <- "Source_Classification_Code.rds"
 dataFileSrcSUM <- "summarySCC_PM25.rds"
-dataFileOut <- "plot1.png"
+dataFileOut <- "plot4.png"
 
 if ((!file.exists(dataFileSrcSCC)) | (!file.exists(dataFileSrcSUM))) {
         if (!file.exists(dataFileZip)) {
                 print("Downloading Data Zip File")
                 if (download.file(dataFileURL, dataFileZip)) {
                         stop(paste("Could not download data file <",
-                                   dataFileURL, "> to zip file <",
-                                   dataFileZip, ">", sep = ""))
+				   dataFileURL, "> to zip file <",
+				   dataFileZip, ">", sep = ""))
                 }
         }
         print("Unzipping Data Source Files")
         unzip(dataFileZip)
-        if ((!file.exists(dataFileSrcSCC)) | (!file.exists(dataFileSrcSUM))) {
+	if ((!file.exists(dataFileSrcSCC)) | (!file.exists(dataFileSrcSUM))) {
                 stop(paste("Could not unzip data zip file <", dataFileZip,
-                           "> to data files <", dataFileSrcSCC, "> and ",
-                           dataFileSrcSUM, ">.", sep = ""))
-        }
+			   "> to data files <", dataFileSrcSCC, "> and ",
+			   dataFileSrcSUM, ">.", sep = ""))
+	}
         print("Data Files Successfully loaded.")
 }
+
+require(ggplot2)
 
 # Extract the data from the data file.
 print("Reading Source Files")
 
 NEI <- readRDS(dataFileSrcSUM)
 SCC <- readRDS(dataFileSrcSCC)
+
+print("Analyzing the Data")
+
+# finding the SCC values from Coal Sources
+SCC <- SCC[grepl("coal", SCC$EI.Sector, ignore.case=TRUE), c("SCC", "EI.Sector")]
+
+# pulling those emissions values that are from Coal sources
+NEI <- NEI[NEI$SCC %in% SCC$SCC,]
 
 print("Creating Plot")
 
@@ -64,7 +72,7 @@ aggdata$Emissions <- aggdata$Emissions / 10^6
 
 # Plot the data. Fiddle with the axis scales to make the plot look nicer
 plot(aggdata,
-     main = "Total PM2.5 Emmissions in the US over Time",
+     main = "Total PM2.5 Emmissions from Coal Sources in the US over Time",
      xlab = "Year",
      ylab = "Emissions (in Mil Tons)",
      pch = 19,
@@ -75,7 +83,7 @@ plot(aggdata,
      xaxt = "n"
 )
 
-# only slow the years we are plotting on the x axis
+# only show labels for the years we are plotting on the x axis
 axis(side = 1, at = aggdata$year)
 
 # add a regression line
